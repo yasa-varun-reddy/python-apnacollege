feb2efa41ff24240af7aa8e7c7d86c251266ecd6
varun reddy <varunreddyyasa@gmail.com>
2025-10-16 17:37:36 -0500

    add

---
+++ variables.py
@@ -0,0 +1,16 @@
+name = "Varun Reddy Yasa"    # string
+age = 26
+price = 150000
+
+print(name,age,price)
+print(type(name))
+print(type(age))
+print(type(price))
+
+
+age1 = 23
+old = False
+a = None
+
+print(type(old))
+print(type(a))
